111ea30c8b5b707f3f661060af4133f74fd94f8b
Emilio Rodríguez <emilio.rodriguez@correo.ucu.edu.uy>
2022-09-22 14:56:51 -0300

    Par e impar Parimeter

--- proyecto/src/Library/Class1.cs
+++ proyecto/src/Library/Class1.cs
@@ -1,5 +1,14 @@
 ﻿namespace Library;
-public class Class1
+public class Paraimeter
 {
+    public bool Espar(int numero){
+        if(numero%2==0)return true;
+        else return false;
 
+    }
+     public bool Impar(int numero){
+        if(numero%2!=0)return true;
+        else return false;
+
+    }
 }
